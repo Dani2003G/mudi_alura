ac27ba3e39d42974c21bfb8a8736deee3b913937
Daniel Gonçalves De Almeida <daniel0202almeida@outlook.com>
2024-11-07 18:46:09 -0300

    Adicao do @Entity, @Id e @GeneratedValue(strategy = GenerationType.IDENTITY)

--- src/main/java/br/com/alura/mvc/mudi/model/Pedido.java
+++ src/main/java/br/com/alura/mvc/mudi/model/Pedido.java
@@ -1,14 +1,23 @@
 package br.com.alura.mvc.mudi.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.Getter;
 import lombok.Setter;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
+@Entity
 @Getter
 @Setter
 public class Pedido {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
     private String nomeProduto;
 
