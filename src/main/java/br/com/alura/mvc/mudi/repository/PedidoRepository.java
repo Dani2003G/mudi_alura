42d176e18e3e4c984080217ae0b1ce2e290b0173
Daniel Gonçalves De Almeida <daniel0202almeida@outlook.com>
2024-11-10 14:10:38 -0300

    Adicao do findByStatusEUsuario

--- src/main/java/br/com/alura/mvc/mudi/repository/PedidoRepository.java
+++ src/main/java/br/com/alura/mvc/mudi/repository/PedidoRepository.java
@@ -2,6 +2,8 @@ package br.com.alura.mvc.mudi.repository;
 
 import br.com.alura.mvc.mudi.model.Pedido;
 import br.com.alura.mvc.mudi.model.StatusPedido;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -12,8 +14,12 @@ import java.util.List;
 @Repository
 public interface PedidoRepository extends JpaRepository<Pedido, Long> {
 
-    List<Pedido> findByStatus(StatusPedido status);
+    @Cacheable("pedidos")
+    List<Pedido> findByStatus(StatusPedido status, Pageable sort);
 
-    @Query("select p from Pedido p join p.user u WHERE u.username = :username")
+    @Query("SELECT p FROM Pedido p JOIN p.user u WHERE u.username = :username")
     List<Pedido> findByUsuario(@Param("username") String username);
+
+    @Query("SELECT p FROM Pedido p JOIN p.user u WHERE p.status = :status AND u.username = :username")
+    List<Pedido> findByStatusEUsuario(@Param("status") StatusPedido statusPedido, @Param("username") String username);
 }
